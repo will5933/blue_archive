fcae6e156615c4599ae6391c630864f5ee1843d4
Moris <will.in@outlook.com>
2023-12-13 00:02:52 +0800

    debug

--- js/script1.js
+++ js/script1.js
@@ -1,28 +1,47 @@
-// if (confirm('是否允许自动播放背景音乐')) {
-//   selector('#start-bgm1').play();
-// }
-
 function selector(str) {
   return document.querySelector(str);
 }
 
+function delay(sec, fn) {
+  setTimeout(fn, sec);
+}
+
+let bgmPlayer = selector('#start-bgm1');
+let lay0 = selector('#lay0');
+let lay1 = selector('#lay1');
+let layLoading = selector('#lay-loading');
 // init
 selector('div#lay-start>img').style.opacity = '1';
-setTimeout(() => {
+delay(2000, () => {
   selector('div#lay-start').style.opacity = '0';
-  setTimeout(() => {
+  delay(1000, () => {
     selector('div#lay-start').style.display = 'none';
     selector('div#phone').append(selector('#popup-tmp').content.cloneNode(true));
 
     // click event
     addButtonClickEffect();
     selector('.confirm').addEventListener('click', () => {
-      selector('#start-bgm1').play();
+      bgmPlayer.play();
       setTimeout(() => selector('.popup-lay').remove(), 100);
     });
     selector('.close-icon').addEventListener('click', () => selector('.popup-lay').remove());
-  }, 1000);
-}, 2000);
+  });
+});
+
+// enter game
+lay1.addEventListener('click', () => {
+  bgmPlayer.volume = 0.3;
+  lay0.style.opacity = '0';
+  lay1.style.opacity = '0';
+  layLoading.style.display = 'unset';
+  
+  delay(300, () => {
+    lay0.remove();
+    lay1.remove();
+    // break
+    delay(3000, () => confirm('To be continued, maybe?'));
+  });
+});
 
 function addButtonClickEffect() {
   document.querySelectorAll('.ba-button').forEach(ele => {
@@ -30,9 +49,9 @@ function addButtonClickEffect() {
       let className = 'button-click-effect';
       if (this.classList.contains('confirm')) className = 'button-click-effect-skew';
       this.classList.add(className);
-      setTimeout(() => {
+      delay(100, () => {
         this.classList.remove(className);
-      }, 100);
+      });
     });
   });
 }
